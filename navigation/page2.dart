d37ffca0a085ebadad6705bb4b797b72d0fe7526
Sathwika-25 <cheralasathwika25@gmail.com>
2025-05-07 10:51:13 +0530

    Create page2.dart

---
+++ navigation/page2.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Page 2")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Pop the current page to go back to the previous page
+            Navigator.pop(context);
+          },
+          child: Text("Pop to Previous Page"),
+        ),
+      ),
+    );
+  }
+}
